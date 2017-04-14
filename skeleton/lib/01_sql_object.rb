2d8d546df358e8c477b075f5b2586617228a1c8c
Sam Wang <sam.zz.wang@gmail.com>
2017-04-14 11:21:57 -0700

    Add skeleton, implement my_attr_accessor, implement setter and getter for columns

---
+++ skeleton/lib/01_sql_object.rb
@@ -0,0 +1,75 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    return @columns if @columns
+    result = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        "#{table_name}"
+    SQL
+
+    @columns = result.map(&:to_sym)
+  end
+
+  def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name ||= self.to_s.tableize
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    @attributes ||= {}
+    @attributes
+  end
+
+  def attribute_values
+    @attributes.values
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
